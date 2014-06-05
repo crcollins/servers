17a56fe7f89afb8d233a4b996f6e5fe08065eaaa
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-06-05 01:00:24 -0400

    Setup databases to use UTF-8 encoding

--- install_database.sh
+++ install_database.sh
@@ -8,8 +8,16 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 EOF
 
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+sudo locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
+
 sudo apt-get install -y postgresql
 
 sudo -u postgres createdb mydb
 sudo -u postgres createuser vagrant -ws
 sudo -u postgres psql -U postgres -d postgres -c "alter user vagrant with password 'vagrant';"
+
