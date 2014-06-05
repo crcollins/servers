db7faef9546f1b0ce554204cfebed9dedae0f60d
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-06-05 01:31:28 -0400

    Update database install script to include server settings

--- install_database.sh
+++ install_database.sh
@@ -21,3 +21,7 @@ sudo -u postgres createdb mydb
 sudo -u postgres createuser vagrant -ws
 sudo -u postgres psql -U postgres -d postgres -c "alter user vagrant with password 'vagrant';"
 
+sudo sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.1/main/postgresql.conf
+sudo /bin/sh -c 'echo "host all all 0.0.0.0/0 md5\nhost all all ::1/0 md5" >> /etc/postgresql/9.1/main/pg_hba.conf'
+
+sudo service postgresql restart
