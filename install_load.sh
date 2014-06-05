079ed8569d8fdb6915b0c4db61971a57a66ba6b1
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-06-05 10:35:08 -0400

    Add vagrant setup for multiple servers with a load balancer

---
+++ install_load.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y nginx
+
+
+cat > /etc/nginx/nginx.conf <<\EOF
+user www-data;
+worker_processes 4;
+pid /var/run/nginx.pid;
+
+events {
+    worker_connections 768;
+    # multi_accept on;
+}
+
+http {
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+
+    log_format upstreamlog '[$time_local] $remote_addr - $remote_user - $server_name  to: $upstream_addr: $request upstream_response_time $upstream_response_time msec $msec request_time $request_time';
+    access_log /var/log/nginx/access.log upstreamlog;
+    error_log /var/log/nginx/error.log;
+
+    gzip on;
+    gzip_disable "msie6";
+
+    include /etc/nginx/conf.d/*.conf;
+    include /etc/nginx/sites-enabled/*;
+
+    upstream myapp1 {
+EOF
+
+for i in `seq 101 $1`; do echo "        server 192.168.1.$i;" >> /etc/nginx/nginx.conf; done
+
+cat >> /etc/nginx/nginx.conf <<\EOF
+    }
+
+    server {
+        listen 80;
+
+        location / {
+            proxy_set_header  Host  $host;
+            proxy_set_header  X-Real-IP $remote_addr;
+            proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_pass http://myapp1;
+        }
+    }
+}
+EOF
+
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo nginx -s reload
+sudo service nginx restart
+
+
+
