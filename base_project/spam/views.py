b9a8f7b8535d3845eb93427e858dcc2d5c25f53a
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-06-13 23:35:01 -0400

    Fix read/write database identification code

--- base_project/spam/views.py
+++ base_project/spam/views.py
@@ -2,6 +2,7 @@ import socket
 
 from django.shortcuts import render
 from django.template import Context
+from django.db import router
 
 from models import Spam, SpamForm
 
@@ -30,11 +31,12 @@ def index(request):
 
 
 def info(request):
+    spam = Spam(name='', text='')
     c = Context({
         "hostname": socket.gethostname(),
         "ip": get_client_ip(request),
-        "read_db": Spam.objects.db,
-        "write_db": Spam(name='', text='')._default_manager.db,
+        "read_db": router.db_for_read(spam.__class__, instance=spam),
+        "write_db": router.db_for_write(spam.__class__, instance=spam),
         })
     return render(request, "spam/info.html", c)
 
